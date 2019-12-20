e78dd83bedc7812370998cbf0f2929a4633eb637
Maria Samsonova <yamaha.11.01.1990@gmail.com>
2019-12-20 16:45:27 +0300

    homework

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+alert ('Are you ready?');
+console.log ('Do you like sport?')
+
+function changeBackground(color) 
+{
+   document.body.style.background = color;
+}
+
+var confDialog = window.confirm("do you want a story??")
+ if (confDialog)
+  console.log ("excellent!")
+ else
+  console.log("Bad:(")
